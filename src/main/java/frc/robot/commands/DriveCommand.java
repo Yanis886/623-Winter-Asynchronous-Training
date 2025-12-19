0a0aaf71041e4d9252f18ffd873e02e7ce0850a5
Lamborghini Bugatti <yanismehenni2009@gmail.com>
2025-12-18 22:14:58 -0500

    Develop BaseCode

---
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+import swervelib.SwerveDrive;
+
+public class DriveCommand extends Command {
+   //variable drivetreain
+   Drivetrain drivetrain;
+   DoubleSupplier xSupplier;
+   DoubleSupplier ySupplier;
+   DoubleSupplier thetaSupplier;
+   
+
+   public DriveCommand(DoubleSupplier xSupplier, DoubleSupplier ySupplier, DoubleSupplier thetaSupplier, Drivetrain drivetrain){
+    this.xSupplier = xSupplier;
+    this.ySupplier = ySupplier;
+    this.thetaSupplier = thetaSupplier;
+    this.drivetrain = drivetrain;
+   }
+//    double getters for the controller x, y, and z
+   
+//hace a constructor to get variables
+    @Override
+    public void end(boolean interrupted){
+
+    }
+    
+
+    public void execute(){
+
+    }
+    public boolean isFinished(){
+        return false;
+    }
+
+    @Override
+    public boolean runsWhenDisabled(){
+        return false;
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,74 @@
+package frc.robot.subsystems;
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+
+public class Drivetrain extends SubsystemBase{
+    SwerveDrive swerveDrive;
+    public Drivetrain(File directory){
+        try{
+        swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.MAX_SPEED,
+           new Pose2d() );
+        } catch (Exception e)    {
+            //in case code up top crashes
+            System.out.println("constructor failed");
+            throw new RuntimeException();
+        }
+    //you candrive now
+    swerveDrive.setMotorIdleMode(true);
+    }
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY,
+      DoubleSupplier angularRotation){
+        return Commands.run(() -> {
+      swerveDrive.driveFieldOriented(new ChassisSpeeds(
+          deadzone(translationX.getAsDouble(), .05)
+              * swerveDrive.getMaximumChassisVelocity(),
+          deadzone(translationY.getAsDouble(), .05)
+              * swerveDrive.getMaximumChassisVelocity(),
+          deadzone(angularRotation.getAsDouble(), .05)
+              * swerveDrive.getMaximumChassisAngularVelocity()),
+          new Translation2d());
+         }, this);
+        }
+         public Pose2d getPose(){
+            return swerveDrive.getPose();
+         }
+
+         public void resetOdometry(Pose2d pose){
+            swerveDrive.resetOdometry(pose);
+         }
+
+         public void zeroGyro(){
+            swerveDrive.zeroGyro();
+         }
+
+         public ChassisSpeeds getRobotVelocity(){
+            return swerveDrive.getFieldVelocity();
+         }
+         public double deadzone(double input, double threshold){
+            if(Math.abs(input)>threshold){
+                return input;
+            } else{
+                return 0.0;
+            }
+
+         }
+
+
+
+
+
+
+
+}
+
