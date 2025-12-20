6193211e67083dc775c456728f269e903ba55b6f
WhosArjun <arjun.arunkumar.v@gmail.com>
2025-12-20 16:52:16 -0500

    Addition of the commands, intitialize, execute, end, isFinished, runsWhenDisabled, getInterruptionBehaviour

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,8 +1,10 @@
 package frc.robot.commands;
 
+import java.util.Set;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.Drivetrain;
 import swervelib.SwerveDrive;
 
@@ -19,26 +21,44 @@ public class DriveCommand extends Command {
     this.ySupplier = ySupplier;
     this.thetaSupplier = thetaSupplier;
     this.drivetrain = drivetrain;
+    addRequirements(drivetrain);
    }
 //    double getters for the controller x, y, and z
    
 //hace a constructor to get variables
+   
+   //Marks the commands start (Calls when the command is scheduled)
+    @Override
+    public void initialize(){
+        
+    }
+    //Called repeatedly when the command is scheduled
+    @Override
+    public void execute(){
+
+    }
+    //Called once when the command ends
     @Override
     public void end(boolean interrupted){
 
     }
-    
-
-    public void execute(){
-
-    }
+    //Returns true whenever the command ends
+    @Override
     public boolean isFinished(){
         return false;
     }
-
-    @Override
+    //Returns whether the robot can run when disabled, method is defaulted to return false to ensure safety.
     public boolean runsWhenDisabled(){
         return false;
     }
+    //Returns which takes priority in conflict 
+    @Override
+    public Command.InterruptionBehavior getInterruptionBehavior(){
+        return InterruptionBehavior.kCancelSelf;
+    }
+
+
+
+
 
 }
